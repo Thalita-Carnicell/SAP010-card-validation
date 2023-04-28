65a786de64520ac5d17b9d5194c46886cc2ed65c
Thalita-Carnicell <thalita_ramos27@hotmail.com>
2023-04-28 10:27:45 -0300

    primeiro commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import validator from './validator.js';
 
+document.getElementById("BotaoPagamento").addEventListener("click",apertandoOBotao)
+function apertandoOBotao(){alert("Aguarde validando cartão....")
+
+const numerosdigitado=document.getElementById("numerosimput").value
+
+if (validator.isValid(numerosdigitado)) {
+    const lastFourDigits = numerosdigitado.substring(numerosdigitado.length - 4);
+    const maskedValue = validator.maskify(numerosdigitado);
+    alert(
+      "Pagamento Aprovado numero de cartao de credito digitado Valido.\nÚltimos 4 dígitos: " +
+        lastFourDigits +
+        "\nValor mascarado: " +
+        maskedValue
+    );
+  } else {
+    alert("Pagamento Não aprovado O número de cartão de crédito é inválido.");
+  }
+}
+
 console.log(validator);
